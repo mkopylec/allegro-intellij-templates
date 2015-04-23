e7432c056b6a7465f44eb5216527241fa3c3679e
mariusz.kopylec <mariusz.kopylec@allegro.pl>
2015-04-23 10:31:28 +0200

    Config props template corrected

--- fileTemplates/$ Spring Boot Configuration Properties Provider.java
+++ fileTemplates/$ Spring Boot Configuration Properties Provider.java
@@ -1,7 +1,9 @@
 #if ($PACKAGE_NAME && $PACKAGE_NAME != "")package $PACKAGE_NAME;#end
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties("${Properties_group_prefix}")
 public class ${NAME} {
     //TODO Add field representations of properties with prefix: '${Properties_group_prefix}' application-*.yml files
